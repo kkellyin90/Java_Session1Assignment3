ba0dba97d48f6c2c84cefd3bddc79927e8ab7720
kkellyin90 <keith.kelly.a@gmail.com>
2016-05-04 12:27:00 +0530

    Added files via upload

---
+++ exampleData.java
@@ -0,0 +1,13 @@
+
+public class exampleData {
+
+	public static void main(String[] args) {
+		
+		//Give two examples of data that cannot be stored in a variable of type int
+		
+		int example1 = 45.5f;
+		int example2 = "acadgild";
+
+	}
+
+}
